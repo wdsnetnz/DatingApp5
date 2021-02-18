8b44d082cd6086f45eec5d9195a16304c4d9eedc
Dinesh Silva <dinesh_silva@dialog.com.au>
2021-02-18 14:54:40 +1000

    Section 4 Just add Account Register

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,10 +6,18 @@ namespace API.Entities
         /// Make the Id, because EntityFrame form will then treat as primary key.
         /// </summary>
         public int Id { get; set; }
+
         /// <summary>
         /// Make as UserName, dotnet authrization map to UserName.
         /// </summary>
         public string UserName { get; set; }
 
+        /**
+        * ? ffff
+        * !!!
+        * TODO: write something here.
+        **/
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
